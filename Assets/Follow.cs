1a8f71591e77c6517af82432f31b58a5ce2d25cd
MrCowFace <59069516+MrCowFace@users.noreply.github.com>
2020-07-02 21:15:59 -0400

    Initial commit

---
+++ Assets/Follow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class Follow : MonoBehaviour
+{
+    [SerializeField]
+    private VisualEffect visualEffect;
+
+
+    public GameObject target;
+
+    public float strength;
+
+    public float distance;
+
+
+    private void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        target.transform.position = ray.GetPoint(distance);
+
+    }
+
+    private void LateUpdate()
+    {
+
+        var pos = target.transform.position - transform.position;
+
+        visualEffect.SetFloat("strength", strength);
+        visualEffect.SetVector3("target", pos);
+        
+    }
+
+
+
+}
